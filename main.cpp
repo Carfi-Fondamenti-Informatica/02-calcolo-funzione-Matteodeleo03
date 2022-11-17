a39d5cf686a7aa361acab17ffa88bc62baeb25ef
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-11-17 14:19:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ if ((x<0) && (y>0)) {
 z = (a*x) - (b*y);
 cout << z << endl;
 } else if ((x>=0) && (y<=0)) {
-z = (a*x*x) - (b*y) 
+z = (a*x*x) - (b*y); 
 cout << z << endl;
 } else {
-z = (a*x) + (b*y*y)
+z = (a*x) + (b*y*y);
 cout << z << endl;
 }
    return 0;
