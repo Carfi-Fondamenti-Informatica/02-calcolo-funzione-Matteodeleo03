3624534392585202b087b851f39ab300fa8b5cc3
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-11-17 15:28:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ float a=0, b=0, x=0, y=0;
 cin >> a >> b >> x >> y;
 if (x<0 && y>0) {
 z = a*x - b*y;
+cout << z << endl;
 } else if (x>=0 && y<=0) {
-z = a*x*x - b*y; 
+z = a*x*x - b*y;
+cout << z << endl;
 } else {
 z = a*x + b*y*y;
-}
 cout << z << endl;
+}
    return 0;
 }
